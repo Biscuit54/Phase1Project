0ec76afbcf5992e084b447f45914c6be233ead26
Jacob Delgadillo <jacobdelgadillo751@gmail.com>
2022-05-30 15:57:18 -0700

    Clap button is functional and positioned properly

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const upThumb = '♥'
+const downThumb = '♡'
+const lVote = '👎'
 const Button = document.getElementById('button')
 button.addEventListener('click', activityTime)
 
@@ -10,3 +13,24 @@ function activityTime() {
             ).innerHTML = `<span class="activityGlam">&nbsp;${data.activity}&nbsp;</span>`
         })
 }
+let btn1 = document.querySelector('#green');
+let btn2 = document.querySelector('#red');
+
+btn1.addEventListener('click', function() {
+  
+    if (btn2.classList.contains('red')) {
+      btn2.classList.remove('red');
+    } 
+  this.classList.toggle('green');
+  
+});
+
+btn2.addEventListener('click', function() {
+  
+    if (btn1.classList.contains('green')) {
+      btn1.classList.remove('green');
+    } 
+  this.classList.toggle('red');
+  
+});
+
